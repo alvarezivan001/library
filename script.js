91de63d0803304dcf85f9d7282ac1c7c0d74ee9d
Ivan Alvarez <alvarezivan001@yahoo.com>
2023-03-21 20:14:27 -0700

    add table logic, (missing form/button logic)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let myLibrary = [];
 
-function Book(title, pages, author, read){
+function Book(title, author, pages, read){
     this.title = title
-    this.pages = pages
     this.author = author
+    this.pages = pages
     this.read = read
     this.info = function(){
       if(read === true)
@@ -14,8 +14,8 @@ function Book(title, pages, author, read){
     }
   }
   
-  const bookOne = new Book("Hobbit", 300, "Tolkien", true);
-  const bookTwo = new Book("Atlas Shrugged", 400, "Rand", false);
+  const bookOne = new Book("Hobbit", "Tolkien", 300, true);
+  const bookTwo = new Book("Atlas Shrugged", "Rand", 400, false);
   
 addBookToLibrary(bookOne);
 addBookToLibrary(bookTwo);
@@ -28,8 +28,45 @@ function addBookToLibrary(book) {
 }
 
 function displayLibrary(){
-    
+        const tableBody = document.querySelector('tbody');
+        myLibrary.forEach(e => {
+            const tableRow = document.createElement('tr');
+            
+            for (const key in e) {
+              
+                if(e[key] == e.title) {tableRow.appendChild(addData(e[key]));}
+                else if(e[key] == e.author) {tableRow.appendChild(addData(e[key]));}
+                else if(e[key] == e.pages) {tableRow.appendChild(addData(e[key]));}
+                else if(e[key] == true) {tableRow.appendChild(addButton('Read'));}
+                else if(e[key] == false) {tableRow.appendChild(addButton('Unread'));}
+                
+                console.log(key);
+            }
+
+            tableRow.appendChild(addButton('Remove'));
+
+            tableBody.appendChild(tableRow);
+
+            
+        });
 }
+
+function addData(data) {
+
+    const tableData = document.createElement('td');
+    tableData.textContent = data;
+    return tableData;
+}
+function addButton(text) {
+  const tableData = document.createElement('td');
+  const tableButton = document.createElement('button')
+  tableButton.textContent = text;
+  tableData.appendChild(tableButton);
+  return tableData;
+
+}
+
+displayLibrary();
   // const variable = 'property'
   // console.log(myObject.variable);
   // console.log(myObject[variable]);
